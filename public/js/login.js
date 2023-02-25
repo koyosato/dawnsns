1d10605f3f7ee07f0341a041689e33bb9d317645
satokoyo <thebeatles.0605@gmail.com>
2023-02-25 17:11:59 +0900

    no message

--- public/js/login.js
+++
@@ -1,5 +0,0 @@
-$(function(){
-  $("button").click(function () {
-    $(".links01").slideToggle();
-  });
-});
---
+++ public/js/script.js
@@ -0,0 +1,21 @@
+$(function(){
+  $("button").click(function () {
+    $(".links01").slideToggle();
+  });
+});
+
+$(function () {
+  $('.modalopen').each(function () {
+    $(this).on('click', function () {
+      var target = $(this).data('target');
+      var modal = document.getElementById(target);
+      console.log(modal);
+      $(modal).fadeIn();
+      return false;
+    });
+  });
+  $('.modalClose').on('click', function () {
+    $('.js-modal').fadeOut();
+    return false;
+  });
+});
